6c61e1c9fb2d4f76e19ac5c51b042e64545339d1
LukaszSzyp <54202919+LukaszSzyp@users.noreply.github.com>
2020-04-04 19:46:45 +0200

    addCode

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -1,2 +1,12 @@
 public class GameOfLife {
+    public static void main(String[] arg) {
+        int[][] board = {
+                {0, 1, 0},
+                {0, 0, 1},
+                {1, 1, 1},
+                {0, 0, 0}
+        };
+        Gra gra1 = new Gra();
+        gra1.gameOfLife(board);
+    }
 }
